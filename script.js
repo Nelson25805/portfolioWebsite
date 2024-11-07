d51f6c5c961783d1593f96e413f303982981b97e
Nelson25805 <91099339+Nelson25805@users.noreply.github.com>
2024-11-07 15:24:41 -0500

    adding modal elements to script page

--- script.js
+++ script.js
@@ -23,3 +23,33 @@ function updateCarousel(projectIndex) {
     carousel.style.transform = `translateX(${offset}%)`;
 }
 
+
+
+
+// Get the modal elements
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImage");
+const captionText = document.getElementById("caption");
+const closeBtn = document.querySelector(".close");
+
+// Add click event listeners to all images within the carousel
+document.querySelectorAll('.carousel-images img').forEach(img => {
+    img.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.textContent = this.alt;
+    }
+});
+
+// Close the modal when the 'x' button is clicked
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+};
+
+// Close the modal when clicking outside the image
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
+
